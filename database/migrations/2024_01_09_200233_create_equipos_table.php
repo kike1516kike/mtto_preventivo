5a545c13257ed407713d196f3092134738c2de45
Enrique López <manuel1516enrique@gmail.com>
2024-01-09 14:06:42 -0600

    migraciones

---
+++ database/migrations/2024_01_09_200233_create_equipos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('equipos', function (Blueprint $table) {
+            $table->increments('id_equipo');
+            $table->datetime('fecha_ingreso_equipo');
+            
+            $table->unsignedInteger('id_marca');
+            $table->foreign('id_marca')->references('id_marca')->on('marcas')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('equipos');
+    }
+};
