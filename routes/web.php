c765df7743be9ba7b052385baabe8f2acd6783ba
kike1516kike <manuel1516enrique@gmail.com>
2024-03-11 14:14:51 -0600

    seguridad

--- routes/web.php
+++ routes/web.php
@@ -9,15 +9,21 @@ use Illuminate\Support\Facades\Auth;
 //     return view('welcome');
 // });
 
-Route::get('/', [LoginController::class, 'index'])->name('index')->middleware('guest');
-Route::post('/', [LoginController::class, 'login'])->name('login')->middleware('guest');
-Route::view('home', 'home')->name('home')->middleware('auth');
 
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
-
-
+Route::middleware(['guest'])->group(function () {
+    Route::get('/', [LoginController::class, 'index'])->name('index');
+    Route::post('/', [LoginController::class, 'login'])->name('login');
+});
 
 
+
+Route::middleware('auth')->group(function () {
+    Route::view('home', 'home')->name('home');
+
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    // Agrega aquí otras rutas que requieran autenticación
+});
 
 // Route::post('/login', [LoginController::class, 'login']);
 // Route::get('/', [LoginController::class, 'index'])->name('index')->middleware('guest');
